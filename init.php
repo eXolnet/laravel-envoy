456beaa6282855d112181028ff22037d7ad20e8a
Alexandre D'Eschambeault <xel1045@gmail.com>
2015-07-16 12:02:51 -0400

    Update init.php

--- init.php
+++ init.php
@@ -1,3 +1,16 @@
 <?php
 
-$__container->load(__DIR__.'/Envoy.blade.php', $__compiler, $__data, $__serversOnly);
+$__current_cwd = getcwd();
+chdir(__DIR__);
+
+$__data['__current_cwd'] = $__current_cwd;
+
+$__container->writeCompiledEnvoyFile(
+	$__compiler, __DIR__.'/Envoy.blade.php', $__serversOnly
+);
+
+include getcwd().'/Envoy.php';
+
+@unlink(getcwd().'/Envoy.php');
+
+chdir($__current_cwd);
