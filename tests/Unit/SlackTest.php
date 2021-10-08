91d265556ae5dac7d61b5d622fb1bab032f89a4a
Christophe Tremblay <ctremblay@exolnet.com>
2021-10-08 08:50:37 -0400

    Improved test coverage from 69% to 82%
    Ref #33327

--- tests/Unit/SlackTest.php
+++ tests/Unit/SlackTest.php
@@ -5,6 +5,8 @@ namespace Exolnet\Envoy\Tests\Unit;
 use Exolnet\Envoy\ConfigDeploy;
 use Exolnet\Envoy\ConfigEnvironment;
 use Exolnet\Envoy\Slack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Server\Server;
 use InvalidArgumentException;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
@@ -17,7 +19,7 @@ class SlackTest extends TestCase
     public function setUp(): void
     {
         $this->slack = new Slack(
-            'TheHook',
+            Server::$url,
             'TheChannel',
             'TheTask',
             'TheProject',
@@ -68,5 +70,69 @@ class SlackTest extends TestCase
         $deploy->shouldReceive('getTimeTotal')->once()->andReturn(1);
 
         $this->assertInstanceOf(Slack::class, Slack::make($environment, $deploy, $task));
+    }
+
+    /**
+     * @test
+     * @return void
+     * @throws \ReflectionException
+     */
+    public function testBuildPayload(): void
+    {
+        $getSystemUser = new \ReflectionMethod(Slack::class, 'getSystemUser');
+        $getSystemUser->setAccessible(true);
+        $systemUser = $getSystemUser->invoke($this->slack);
+
+        $fields = [
+            [
+                'title' => 'Commit',
+                'value' => 'TheCommit',
+                'short' => true,
+            ],
+            [
+                'title' => 'Release',
+                'value' => 'TheRelease',
+                'short' => true,
+            ],
+            [
+                'title' => 'URL',
+                'value' => 'TheAppUrl',
+                'short' => false,
+            ]
+        ];
+
+        $arrayFilter = [
+            'channel' => 'TheChannel',
+            'attachments' => [
+                [
+                    'color' => 'good',
+                    'text' => '*[TheProject]* _' . $systemUser . '_ ran `TheTask` on _TheEnv_ in 0 seconds.',
+                    'mrkdwn_in' => ['text'],
+                    'fields' => $fields,
+                ],
+            ],
+        ] ;
+
+        $buildPayload = new \ReflectionMethod(Slack::class, 'buildPayload');
+        $buildPayload->setAccessible(true);
+
+        $this->assertEquals($arrayFilter, $buildPayload->invoke($this->slack));
+    }
+
+    /**
+     * @test
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Exception
+     */
+    public function testSend(): void
+    {
+        Server::enqueue([
+            new Response(200),
+        ]);
+
+        $this->slack->send();
+
+        $this->expectOutputString('Slack notification sent.'. PHP_EOL);
     }
 }
